f04a9404d7c0acdaed9950d3bbbe55aca898aa45
markpollente <smertkhod@gmail.com>
2025-02-06 15:25:40 +0800

    Remove import wildcard on TableService

--- service/src/test/java/com/mvnmulti/TableServiceTest.java
+++ service/src/test/java/com/mvnmulti/TableServiceTest.java
@@ -13,10 +13,17 @@ import org.mockito.Mockito;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashSet;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class TableServiceTest {
 
